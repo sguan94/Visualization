e4a8c3ee008164d60d0491d48523b3af37f4e5b1
Sijie <sguan@ucalgary.ca>
2019-08-16 22:02:44 -0600

    3

---
+++ src/components/ToolBar/Toolbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './Toolbar.css';
+import DrawerToggleButton from "../SideDrawer/DrawerToggleButton";
+
+const Toolbar = props => (
+    <header className="toolbar">
+        <nav className="toolbar__navigation">
+            <div>
+                <DrawerToggleButton click={props.drawerClickHandler}/>
+            </div>
+            <div className="toolbar__logo"><a href="/">THE LOGO</a></div>
+            <div className="spacer"></div>
+            <div className="toolbar__navigation-items">
+                <ul>
+                    <li><a href="/">Products</a></li>
+                    <li><a href="/">Users</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default Toolbar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import React from "react";
 import { render } from "react-dom";
+import Toolbar from './components/ToolBar/Toolbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import BackDrop from './components/BackDrop/BackDrop'
 
 class App extends React.Component {
+
+    state = {
+        sideDrawerOpen: false
+    };
+
+    drawerToggleClickHandler = () => {
+        this.setState((prevState) => {
+            return {sideDrawerOpen : !prevState.sideDrawerOpen}
+        });
+    };
+
+    backDropClickHandler = () => {
+        this.setState((prevState) => {
+            return {sideDrawerOpen : false}
+        })
+    }
+
     render() {
+        let backDrop;
+        if(this.state.sideDrawerOpen){
+            backDrop = <BackDrop click={this.backDropClickHandler}/>;
+        }
         return (
-            <div>
-                <h1>HELLO!</h1>
+            <div style={{height: '100%'}}>
+                <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
+                <SideDrawer show={this.state.sideDrawerOpen}/>
+                {backDrop}
+                <main style={{marginTop: '64px'}}>
+                    <p>dfhgfdfsgfnwesgdhesgdgd</p>
+                </main>
             </div>
         )
     }
